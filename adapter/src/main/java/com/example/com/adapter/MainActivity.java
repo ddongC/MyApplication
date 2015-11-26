11287be5c3cd1f5b0f6ffbe2ded8510dfe8ca3c8
ddongC <optu123@naver.com>
2015-11-26 11:28:40 +0900

    exex

---
+++ adapter/src/main/java/com/example/com/adapter/MainActivity.java
@@ -0,0 +1,103 @@
+package com.example.com.adapter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.Objects;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("그리드 뷰 영화 포스터");
+
+        final GridView gv = (GridView)findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+    }
+
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+        Integer[] posterID = {
+                R.drawable.movie0, R.drawable.movie1, R.drawable.movie2,
+                R.drawable.movie3, R.drawable.movie4, R.drawable.movie5,
+                R.drawable.movie6, R.drawable.movie7, R.drawable.movie8,
+                R.drawable.movie9
+        };
+
+        public MyGridAdapter(Context c) {
+            context = c;
+        }
+
+        public int getCount() {
+            return posterID.length;
+        }
+
+        public Object getItem(int arg0) {
+            return null;
+        }
+
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ImageView imageview = new ImageView(context);
+            imageview.setLayoutParams(new GridView.LayoutParams(100, 150));
+            imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageview.setPadding(5, 5, 5, 5);
+            imageview.setImageResource(posterID[position]);
+            final int pos = position;
+
+            imageview.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    View dilogView = (View) View.inflate(MainActivity.this, R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dilogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[pos]);
+                    dlg.setTitle("큰 포스터");
+                    dlg.setIcon(R.drawable.images);
+                    dlg.setView(dilogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+            return imageview;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
